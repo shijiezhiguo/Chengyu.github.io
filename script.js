f48e63aead778ac61d42e7bd3957def5c9e63d26
shijiezhiguo <84444051+shijiezhiguo@users.noreply.github.com>
2023-08-03 08:50:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function tophat() {
+    window.location.href = "https://app.tophat.com/e/776977"
+}
+
+function drive() {
+    window.location.href = "https://drive.google.com/drive/folders/1Ak_kUW07RMNZgiEnxxbbLKjSg__lKE6j"
+}
+
+function kurose() {
+    window.location.href = "https://drive.google.com/drive/folders/1RYCRYD285oGdFCZDq9eqApOpA5UcELMJ"
+}
+
+function w3schools() {
+    window.location.href = "http://w3schools.com"
+}
+
+function geeks() {
+    window.location.href = "https://www.geeksforgeeks.org/web-development/ "
+}
+
+document.getElementById("width").innerHTML = "Browser inner window width: " + window.innerWidth + "px";
+document.getElementById("height").innerHTML = "Browser inner window height: " + window.innerHeight + "px";
+document.getElementById("screenWidth").innerText = "Screen window width: " + window.screen.width + " px";
+document.getElementById("screenHeight").innerText = "Screen window height: " + window.screen.height + " px";
+document.getElementById("availableWidth").innerText = "Screen window available width: " + window.screen.availWidth + " px";
+document.getElementById("availableHeight").innerText = "Screen window available height: " + window.screen.availHeight + " px";
+document.getElementById("colorDepth").innerText = "Screen window color depth: " + window.screen.colorDepth + " bits";
+document.getElementById("pixelDepth").innerText = "Screen window pixel depth: " + window.screen.pixelDepth + " bits";
